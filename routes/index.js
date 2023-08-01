9e84082c8be150e6d29aa950c90290a27a4a7bbb
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-31 22:51:33 -0500

    Added helpers file, added pseudo coding and completed routes on index.js

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Import modular routers for notes
+const notesRouter = require('./notes')
+
+// Variable for app = express()
+const app = express();
+
+// app use for each router
+app.use('/notes', notesRouter);
+
+// export app
+module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,16 @@
+// Import all necessary files
+
+// GET Route for retriving all the notes
+
+// GET route for a specific note
+// 
+
+// DELETE route for specific note - OPTIONAL
+
+// POST route for a new note
+    // const for req.body
+    // if req.body
+    // readAndAppend
+
+
+// export notes
